ca56939042deba0aecc70188703c68c17aad8f23
addiken <addiken@mail.ru>
2021-03-03 20:49:50 +0300

    lab2

---
+++ lab2.rb
@@ -0,0 +1,78 @@
+# Получение введенного значения
+def inputWithCondition (str, is_int)
+  if is_int
+    print "Введите #{str}: "
+    value = gets.to_i
+  else
+    print "Введите #{str}: "
+    value = gets.chomp
+  end
+  value
+end
+
+# Скрипт №1
+def script1
+  str = inputWithCondition("строку", false)
+  i = inputWithCondition("число", true)
+  if str[-2, 2] == ('CS')
+    puts("2 ^ #{i} = #{2**i}")
+  else
+    puts("#{str} наоборот --> #{str.reverse}")
+  end
+end
+
+def script2
+  pokemons = []
+  iterations = inputWithCondition('количество покемонов', true)
+  i = 0
+  while i < iterations do
+    _name = inputWithCondition('имя покемона', false)
+    _color = inputWithCondition('цвет покемона', false)
+    pokemon = { "name" => _name, "color" => _color }
+    pokemons << pokemon
+    i+=1
+  end
+  puts('----------------------------------------------------------------------')
+  puts(pokemons)
+end
+
+def main
+  loop do
+    puts 'Выберите, что хотите сделать:'
+    print("\n")
+    puts "1 - Cкрипт, который принимает число и слово, если слово
+заканчивается на “CS” - выводит на экран цифру 2 в степени (длины введенного
+слова), если не заканчивается - выводит слово задом наперед"
+    print("\n")
+    puts '2 - Скрипт, который выводит массив покемонов.'
+    print("\n")
+    puts 'q - Выход'
+    print("\n")
+    i = gets.chomp
+    # system('cls')
+    case i
+    when '1'
+      print("\n")
+      print("======================================================================\n")
+      print("\n")
+      # Скрипт №1
+      script1
+      print("\n")
+      print("======================================================================\n")
+      print("\n")
+    when '2'
+      print("\n")
+      print("======================================================================\n")
+      print("\n")
+      # Скрипт №2
+      script2
+      print("\n")
+      print("======================================================================\n")
+      print("\n")
+    when 'q'
+      break
+    end
+  end
+end
+
+main
