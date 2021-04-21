ea49aafc496129f1d78b7440ceb3f333bc997948
alexb <alexbalashov201377@mail.ru>
2021-04-21 23:41:27 +0300

    trash code

--- lab5.rb
+++ lab5.rb
@@ -17,25 +17,19 @@ class CashMachine
   end
 
   def withdraw(withdraw_value)
-    if withdraw_value.negative?
-      'Ошибка! Введено отрицательное число.'
-    end
-    if @balance < withdraw_value
-      'Ошибка! Нехватает средств.'
-    end
+    return 'Ошибка! Введено отрицательное число.' if withdraw_value.negative?
+    return 'Ошибка! Нехватает средств.' if @balance < withdraw_value
     @balance -= withdraw_value
-    balance_value = File.open('balance.txt', 'w') 
+    balance_value = File.open('balance.txt', 'w')
     balance_value.puts(@balance)
     balance_value.close
     "New balance: #{@balance}"
   end
 
-  def deposit(deposite_value) 
-    if deposite_value.negative?
-      'Ошибка! Введено отрицательное число.'
-    end
-    @balance += deposite_value
-    balance_value = File.open('balance.txt', 'w') 
+  def deposit(deposit_value)
+    return 'Ошибка! Введено отрицательное число.' if deposit_value.negative?
+    @balance += deposit_value
+    balance_value = File.open('balance.txt', 'w')
     balance_value.puts(@balance)
     balance_value.close
     "New balance: #{@balance}"
@@ -54,21 +48,24 @@ while (connection = server.accept)
   request = connection.gets
   m, full_path = request.split(' ')
   path = full_path.split('/')[1]
-  method = path.split('?')[0]
-  value = path.split('?')[1].split('=')[1].to_i
+  if full_path.split('/')[1].include?('?')
+    method = path.split('?')[0]
+    value = path.split('?')[1].split('=')[1].to_i
+  end
 
   connection.print "HTTP/1.1 200\r\n"
   connection.print "Content-Type: text/html\r\n"
   connection.print "\r\n"
+  connection.print cash_machine.balance if path =='balance'
 
-  connection.print case method
-                   when 'deposit'
-                     cash_machine.deposit(value)
-                   when 'withdraw'
-                     cash_machine.withdraw(value)
-                   when 'balance'
-                     cash_machine.balance
-                   else
-                     'error'
-                   end
+  unless value.nil?
+    connection.print case method
+                     when 'deposit'
+                       cash_machine.deposit(value)
+                     when 'withdraw'
+                       cash_machine.withdraw(value)
+                     else
+                       'error'
+                     end
+  end
 end
